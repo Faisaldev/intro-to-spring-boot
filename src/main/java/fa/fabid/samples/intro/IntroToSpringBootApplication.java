f3fdb98d31112828ebc80f5d6d0d0d5255043ac5
Faisal Ahmed <faisal.dev@gmail.com>
2020-06-30 20:41:49 +0300

    Initial commit

---
+++ src/main/java/fa/fabid/samples/intro/IntroToSpringBootApplication.java
@@ -0,0 +1,13 @@
+package fa.fabid.samples.intro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntroToSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IntroToSpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fa/fabid/samples/intro/IntroToSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package fa.fabid.samples.intro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntroToSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
